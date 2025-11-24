43fd50b16a66e674d337d1871391f84c6cf7cfd9
Saketh Pai <paisaketh@gmail.com>
2025-11-24 13:23:55 +0530

    gamedev packages

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,6 +4,17 @@
   # Display server and desktop environment
   services.xserver.enable = true;
 
+  # GPU / OpenGL configuration (recommended for Intel GPUs)
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    extraPackages = with pkgs; [ mesa ];
+  };
+
+  # Use modesetting for modern Intel GPUs (recommended). For older Intel GPUs
+  # you can switch this to "intel" to use xf86-video-intel.
+  services.xserver.videoDrivers = [ "modesetting" ];
+
   # KDE Plasma 6
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
--- modules/packages.nix
+++ modules/packages.nix
@@ -9,6 +9,20 @@
     vscode
     git
     nodejs
+    godot
+    blender
+    gimp
+    imagemagick
+    ffmpeg
+    sdl2
+    sdl2_image
+    sdl2_mixer
+    sdl2_ttf
+    assimp
+    openal
+    vulkan-loader
+    vulkan-tools
+    python3Packages.pygame
     spotify
     python3
   ];
